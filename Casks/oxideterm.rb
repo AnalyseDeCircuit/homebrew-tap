a19793f2a074d1f755ae55480bc0e0be58e24779
AnalyseDeCircuit <yjmsoleilune@icloud.com>
2026-07-08 22:14:10 +0800

    Automate OxideTerm cask updates

--- Casks/oxideterm.rb
+++ Casks/oxideterm.rb
@@ -1,9 +1,9 @@
 cask "oxideterm" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.6.9"
-  sha256 arm:   "08ea002d946a26a68c269acd7e1827794166be19a8c7d6e83f526cde253d19df",
-         intel: "470259d562a6ca09fb5853b2b073e728ba488b4cb681f281185bde3fb6ef4173"
+  version "1.6.12"
+  sha256 arm:   "056f1b7a36fde84377a418569e3466abcb8bbea114234a8aef2d79638d4b4859",
+         intel: "99ace28d3a9d537cebb32ea088a40e88306ce550d777d4227c1186de0036a9d6"
 
   url "https://github.com/AnalyseDeCircuit/oxideterm/releases/download/v#{version}/OxideTerm_#{version}_macos_#{arch}.dmg"
   name "OxideTerm"
